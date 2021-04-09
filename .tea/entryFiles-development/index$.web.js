3cd4fbdac2f94044a108f1635ed81b0ca5af69f8
liangqifei <1454554357@qq.com>
2021-04-10 02:12:22 +0800

    demo显示

---
+++ .tea/entryFiles-development/index$.web.js
@@ -0,0 +1,6 @@
+require('@alipay/appx-compiler/lib/sjsEnvInit');
+require('./config$');
+
+require('../../node_modules/@antv/my-f2/es/index?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
+require('../../pages/index/index?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/demo1/demo1?hash=83a94e2340ddd13e96f290b8b98de3fd90122450');
